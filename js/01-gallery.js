12a07ccb33e4632a524195c3f3f1accd7ef22820
NilKad <kadulin@gmail.com>
2022-09-20 11:43:48 +0300

    dz7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const createGalleryMarkup = gallery =>
+  gallery
+    .map(
+      ({ original, preview, description }) => `
+  <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join('');
+
+const openIMGmodal = url => {
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+
+  instance.show(() => {
+    galleryRef.addEventListener('keydown', e => {
+      if (e.code === 'Escape' && instance.visible()) {
+        instance.close();
+      }
+    });
+  });
+};
+
+const onClickImg = e => {
+  e.preventDefault();
+  if (e.target.className != 'gallery__image') {
+    return;
+  }
+  openIMGmodal(e.target.dataset.source);
+};
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryRef.addEventListener('click', onClickImg);
+
+// const closeGalletyOnEscapeEvent = () => {
+//   galleryRef.addEventListener('keydown', e => {
+//     console.log(e.code);
+//     if (e.code === 'Escape' && instance.visible()) {
+//       instance.close();
+//     }
+//   });
+// };
+// console.log(createGalleryMarkup(galleryItems));
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+let lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionClass: 'gallery__captionClass',
+});
+
+const createGalletyMarkup = gallery =>
+  gallery
+    .map(
+      ({ original, preview, description }) =>
+        `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`
+    )
+    .join('');
+
+galleryRef.innerHTML = createGalletyMarkup(galleryItems);
+
+lightbox.refresh();
+
+// document.addEventListener('keydown', e => {
+//   if (e.code === 'KeyQ') {
+//     console.log('keyQ: ' + e.code);
+//     lightbox.close('close.simplelightbox', function () {
+//       // do something…
+//     });
+//   }
+//   console.log(e);
+// });
